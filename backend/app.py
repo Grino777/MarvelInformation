8e9d6a0b6d4ab3d6a511840d357821bff2bb2309
grino777 <grino777@gmail.com>
2023-11-22 15:00:41 +0300

    Update project settings

--- backend/app.py
+++ backend/app.py
@@ -8,5 +8,8 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
+# port_number = 5000
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True , port=port_number)
+    app.run(debug=True, host='0.0.0.0')
